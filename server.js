9721c4ccd85f1098c70841e1645a3ca7f8bc595b
AllenPandey7 <AllenPandey@my.unt.edu>
2025-11-04 15:52:03 -0600

    Added render.yaml and updated .NET/portfolio files

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 3000;
 
 // Set EJS as the view engine
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'Views'));
 
 // Use express-ejs-layouts
 app.use(expressLayouts);
